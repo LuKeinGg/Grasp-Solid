96aa541cd2c3d1f918df15d4bdde2e3c01718eda
LuKeinGg <matias.alves2002@gmail.com>
2023-09-11 17:05:52 -0300

    Parte 1

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -25,6 +25,28 @@ namespace Full_GRASP_And_SOLID.Library
             this.steps.Remove(step);
         }
 
+        // Se utiliza el princpio SRP ya que asigno la responsabilidad de caclular el costo de producción a un metodo        
+        // separado, en este caso GetProductionCost()        
+
+        public double GetProductionCost()
+        {
+            double costInsumos = 0;
+            double costEquipamiento = 0;
+
+            foreach (Step step in this.steps)
+            {
+                // Calculo el costo de insumos 
+                costInsumos += step.Input.UnitCost * step.Quantity;
+
+                // Calculo el costo de equipamiento 
+                costEquipamiento += (step.Time / 60) * step.Equipment.HourlyCost;
+            }
+
+            // Calculo el costo total
+            double totalCost = costInsumos + costEquipamiento;
+            return totalCost;
+        }
+
         public void PrintRecipe()
         {
             Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
@@ -33,6 +55,10 @@ namespace Full_GRASP_And_SOLID.Library
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
+
+            // Aquí muestro el costo total de producción
+            double productionCost = GetProductionCost();
+            Console.WriteLine($"Costo total de producción: {productionCost:C}");
         }
     }
 }
